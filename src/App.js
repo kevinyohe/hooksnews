03889e3343b67bdecd35ca92ccfa5975bca3ba31
kevinyohe <kevinyohe7@gmail.com>
2020-01-07 22:31:58 -0500

    refactor useEffect

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import axios from 'axios';
 
 function App() {
   const [results, setResults] = useState([])
+
+
   useEffect(() => {
-    axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
-    .then(response =>{console.log(response);
-    setResults(response.data.hits);})
+    getResults();
+    
+  }, [])
+ 
+
+const getResults = async  () =>{
   
-  }, []);
+    const response = await axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks');
+    setResults(response.data.hits);
+}
 
   return (
    <> 
