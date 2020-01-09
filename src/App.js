d96fedb3e4c40d2a3643b56f4e5b45663acb32ad
kevinyohe <kevinyohe7@gmail.com>
2020-01-08 22:10:13 -0500

    add search input

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import axios from 'axios';
 
 function App() {
   const [results, setResults] = useState([])
-
+  const [query, setQuery] = useState("reacthooks");
 
   useEffect(() => {
     getResults();
     
-  }, [])
+  }, [query])
  
 
 const getResults = async  () =>{
   
-    const response = await axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks');
+    const response = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`);
     setResults(response.data.hits);
 }
 
   return (
    <> 
+   <input type="text" onChange={event=>setQuery(event.target.value)}/>
     {results.map(result => (
       <li key={result.objectID}>
       <a href={result.url}>{result.title}</a>
